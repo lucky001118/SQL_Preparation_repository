8e9bb827915311ecb43965744ce1303cc0313e05
lucky001118 <manikpurilucky218@gmail.com>
2025-09-17 00:45:42 +0530

    update the joins file

--- Joins/JOINS Practice.sql
+++ Joins/JOINS Practice.sql
@@ -92,6 +92,15 @@ DESC teachers;
 DESC enrollments;
 DESC course_teacher;
 
+SELECT * from students;
+SELECT * FROM enrollments;
+SELECT * FROM courses;
+SELECT * FROM course_teacher;
+SELECT * FROM teachers;
+
+INSERT INTO teachers(name) values('Vishal Chaorasia');
+INSERT INTO courses(course_name,credits) VALUES ('English Litreture',3);
+INSERT INTO students(name,age,email) Values ('Domendra Nirmalkar',21,'nirmalkardomendra@gmail.com');
 
 -- Basic (INNER JOIN)
 -- 1.	List all students along with the courses they are enrolled in.
@@ -127,10 +136,33 @@ INNER JOIN courses c ON e.enrollment_id = c.course_id
     
 -- LEFT JOIN
 -- 6.	Show all students and their enrolled courses (include students with no enrollment).
+SELECT s.name, c.course_name
+FROM students s
+LEFT JOIN enrollments e ON s.student_id = e.student_id
+LEFT JOIN courses c ON c.course_id = e.course_id;
+
 -- 7.	Display all courses and the students enrolled in them (include courses with no students).
+SELECT s.name, c.course_name
+FROM courses c 
+LEFT JOIN enrollments e ON c.course_id = e.course_id
+LEFT JOIN students s ON s.student_id = e.student_id;
+
 -- 8.	Show all teachers and their assigned courses (include teachers not assigned to any course).
+SELECT t.name, c.course_name
+FROM teachers t 
+LEFT JOIN course_teacher ct ON t.teacher_id = ct.teacher_id
+LEFT JOIN courses c ON c.course_id = ct.course_id;
+
 -- 9.	List students and their grades, but also include students who have not received a grade yet.
+SELECT s.name, e.grade 
+FROM Students s 
+LEFT JOIN enrollments e ON s.student_id = e.student_id;
+
 -- 10.	Display all students and courses together (even if not enrolled).
+SELECT s.name, c.course_name
+FROM students s 
+LEFT JOIN enrollments e ON s.student_id = e.student_id
+LEFT JOIN courses c ON c.course_id = e.course_id;
 
 -- RIGHT JOIN
 -- 11.	Show all courses with their enrolled students (include courses even if no students enrolled).
